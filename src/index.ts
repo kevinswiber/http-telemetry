f63e9a427422df82091a6d0fa59c811a1063c851
Kevin Swiber <kswiber@gmail.com>
2022-03-28 14:57:44 -0700

    Adding missing change from Meter refactor.

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ export * from "./http-client-instrumentation";
 export * from "./http-client-patch";
 export * from "./exporters";
 export * from "./meter";
-export {
-  IClientExporter,
-  IClientInterceptor,
-  Timings as Measurements,
-} from "./types";
+export { IClientExporter, IClientInterceptor, Timings } from "./types";
